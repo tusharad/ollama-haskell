2a96360e4c75ee02fc83d8ca29ac179f53834dad
tusharad <tusharadhatrao@gmail.com>
2025-05-26 12:12:00 +0530

    chore: removed unnecessary imports for Ollama module

--- src/Ollama.hs
+++ src/Ollama.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DuplicateRecordFields #-}
+
 {- |
   #Ollama-Haskell#
   This library lets you run LlMs from within Haskell projects. Inspired by `ollama-python`.
@@ -26,8 +27,6 @@ module Ollama
   , embeddingOps
   , EmbeddingOps (..)
   , EmbeddingResp (..)
-
-    -- * Ollama operations
 
     -- ** Copy Models
   , copyModel
@@ -68,33 +67,19 @@ module Ollama
   , RunningModels (..)
   , RunningModel (..)
   , Message (..)
-  , Format(..)
+  , Format (..)
   )
 where
 
 import Data.Ollama.Chat
-  ( ChatOps (..)
-  , ChatResponse (..)
-  , Message (..)
-  , Role (..)
-  , chat
-  , chatJson
-  , defaultChatOps
-  )
-import Data.Ollama.Copy (copyModel, copyModelOps)
-import Data.Ollama.Create (createModel, createModelOps)
-import Data.Ollama.Delete (deleteModel, deleteModelOps)
-import Data.Ollama.Embeddings (embedding, embeddingOps, EmbeddingOps (..), EmbeddingResp (..))
+import Data.Ollama.Common.Types 
+import Data.Ollama.Copy 
+import Data.Ollama.Create 
+import Data.Ollama.Delete 
+import Data.Ollama.Embeddings 
 import Data.Ollama.Generate
-  ( GenerateOps (..)
-  , GenerateResponse (..)
-  , defaultGenerateOps
-  , generate
-  , generateJson
-  )
-import Data.Ollama.List (ModelInfo (..), Models (..), list, listOps)
-import Data.Ollama.Ps (RunningModel (..), RunningModels (..), ps, psOps)
-import Data.Ollama.Pull (pull, pullOps)
-import Data.Ollama.Push (push, pushOps)
-import Data.Ollama.Show (ShowModelResponse (..), ShowModelInfo (..), showModel, showModelOps)
-import Data.Ollama.Common.Types (Format (..), ModelDetails (..))
+import Data.Ollama.List 
+import Data.Ollama.Ps 
+import Data.Ollama.Pull 
+import Data.Ollama.Push 
+import Data.Ollama.Show 
