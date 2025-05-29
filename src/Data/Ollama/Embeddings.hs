e7ee3074650fdff8f9f221a9c4aea82d642524f9
tusharad <tusharadhatrao@gmail.com>
2025-05-29 18:08:02 +0530

    feat: added modelOption field in embeddings

--- src/Data/Ollama/Embeddings.hs
+++ src/Data/Ollama/Embeddings.hs
@@ -10,18 +10,21 @@ module Data.Ollama.Embeddings
   ) where
 
 import Data.Aeson
+import Data.Ollama.Common.Config (OllamaConfig)
+import Data.Ollama.Common.Error (OllamaError)
 import Data.Ollama.Common.Utils as CU
 import Data.Text (Text)
-import Data.Ollama.Common.Error (OllamaError)
-import Data.Ollama.Common.Config (OllamaConfig)
+import Data.Ollama.Common.Types (ModelOptions)
 
-defaultEmbeddingOps :: EmbeddingOps 
-defaultEmbeddingOps = EmbeddingOps {
-    model = "llama3.2"
- , input = ""
- , truncateInput = Nothing
- , keepAlive = Nothing
-}
+defaultEmbeddingOps :: EmbeddingOps
+defaultEmbeddingOps =
+  EmbeddingOps
+    { model = "llama3.2"
+    , input = ""
+    , truncateInput = Nothing
+    , keepAlive = Nothing
+    , modelOptions = Nothing
+    }
 
 -- TODO: Add Options parameter
 data EmbeddingOps = EmbeddingOps
@@ -29,6 +32,7 @@ data EmbeddingOps = EmbeddingOps
   , input :: !Text
   , truncateInput :: !(Maybe Bool)
   , keepAlive :: !(Maybe Int)
+  , modelOptions :: !(Maybe ModelOptions)
   }
   deriving (Show, Eq)
 
@@ -45,12 +49,13 @@ instance FromJSON EmbeddingResp where
       <*> v .: "embeddings"
 
 instance ToJSON EmbeddingOps where
-  toJSON (EmbeddingOps model_ input_ truncate' keepAlive_) =
+  toJSON (EmbeddingOps model_ input_ truncate' keepAlive_ ops) =
     object
       [ "model" .= model_
       , "input" .= input_
       , "truncate" .= truncate'
       , "keep_alive" .= keepAlive_
+      , "options" .= ops
       ]
 
 -- TODO: Add Options parameter
@@ -66,9 +71,11 @@ embeddingOps ::
   -- | Keep Alive
   Maybe Int ->
   -- | Ollama Config
+  Maybe ModelOptions ->
+  -- | Model options
   Maybe OllamaConfig ->
   IO (Either OllamaError EmbeddingResp)
-embeddingOps modelName input_ mTruncate mKeepAlive mbConfig = do
+embeddingOps modelName input_ mTruncate mKeepAlive mbOptions mbConfig = do
   withOllamaRequest
     "/api/embed"
     "POST"
@@ -78,12 +85,14 @@ embeddingOps modelName input_ mTruncate mKeepAlive mbConfig = do
           , input = input_
           , truncateInput = mTruncate
           , keepAlive = mKeepAlive
+          , modelOptions = mbOptions
           }
     )
     mbConfig
     commonNonStreamingHandler
 
 -- Higher level binding that only takes important params
+
 -- | Embedding API
 embedding ::
   -- | Model
@@ -92,4 +101,4 @@ embedding ::
   Text ->
   IO (Either OllamaError EmbeddingResp)
 embedding modelName input_ =
-  embeddingOps modelName input_ Nothing Nothing Nothing
+  embeddingOps modelName input_ Nothing Nothing Nothing Nothing
