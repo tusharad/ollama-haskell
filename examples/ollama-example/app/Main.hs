7d14adc5428241f4c375b5286e3b980d44dc12c0
Tushar Adhatrao <tusharadhatrao@gmail.com>
2026-08-04 00:15:41 +0530

    refactor: remove redundant ollama-example project directory and clean up workspace files

--- examples/ollama-example/app/Main.hs
+++
@@ -1,8 +0,0 @@
-module Main (main) where
-
--- import qualified Example.KnowledgeApp as KA
--- import qualified Example.GenerateThinking as Ex
-import Example.ChatStructuredOutput
-
-main :: IO ()
-main = runApp
